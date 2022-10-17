06465b29557e9399b5a27e14247163574fd61ee0
Denis280303 <kozlovskyidenus@gmail.com>
2022-10-17 13:06:35 +0300

    Fix deploy.rb config

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,15 +1,18 @@
 # config valid for current version and patch releases of Capistrano
 lock "~> 3.17.1"
 
+
 set :application, "qna"
 set :repo_url, "git@github.com:Denis280303/qna.git"
+set :branch, "main"
 
 # Default branch is :master
-ask :main, `git rev-parse --abbrev-ref HEAD`.chomp
+# ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
 
 # Default deploy_to directory is /var/www/my_app_name
 set :deploy_to, "/home/deployer/qna"
 set :deploy_user, 'deployer'
+
 
 # Default value for :format is :airbrussh.
 # set :format, :airbrussh
