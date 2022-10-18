b0d794518c4e1f20ebe954138197e0d25b52fc7b
Denis280303 <kozlovskyidenus@gmail.com>
2022-10-18 17:59:05 +0300

    Delete comments format

--- app/javascript/channels/comment_channel.js
+++ app/javascript/channels/comment_channel.js
@@ -8,21 +8,4 @@ consumer.subscriptions.create("CommentChannel", {
     this.perform('follow', { question_id: gon.params['id'] })
   },
 
-  received(data) {
-    console.log(gon.params)
-    console.log(gon.user_id)
-    console.log(data)
-    console.log(data.user_id)
-    console.log(data.comment.commentable_type === 'Question')
-
-    if (!(gon.user_id === data.comment.user_id)) {
-      let commentTemplate = require('templates/comment.hbs')({ comment: data.comment })
-
-      if (data.comment.commentable_type === 'Question') {
-        $('.question-comments').append(commentTemplate)
-      } else {
-        $('.answer-comments').append(commentTemplate)
-      }
-    }
-  }
 });
